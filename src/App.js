af80e6152ba8eb18dce7fc3f903ecf6793e7a0ad
Harathi1428 <harathiyenni@gmail.com>
2023-12-04 18:20:55 +0530

    first commit here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [item,setItem]=useState("");
+  const [items,setItems]=useState([]);
+  function add(event){
+    setItem(event.target.value);
+  }
+  const additem=()=>{
+    setItems((prev)=>{
+      return [...prev,item];
+    })
+    setItem("");
+  }
+  const deleteitem=(index)=>{
+    setItems((prev)=>{
+const newitems=[...prev];
+newitems.splice(index,1);
+return newitems;
+    });
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className='main'>
+    <div className="outer">
+    <div className='heading'><h1><b>to-do</b></h1></div>
+    <div className='text-box'>
+    <input type="text" placeholder='add an item' onChange={add} value={item}/>
+    <button  className="btn" type="submit" onClick={additem}>add</button>
     </div>
-  );
-}
-
+    <div className='item'><ul>
+      {items.map((it,index)=><li key={index}>
+        <h3>{it}</h3>
+          <button className='del' onClick={() => deleteitem(index)}>delete</button>
+        </li>)}</ul></div></div>
+        </section>
+  )}
 export default App;
